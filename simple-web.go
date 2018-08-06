68723a4dce91ad02bf45d323a891b69a5ce4a6ba
JooYoungHeo <jyhur900226@naver.com>
2018-08-06 23:43:17 +0900

    go server - response json simple test

--- simple-web.go
+++ simple-web.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 )
+
+type Member struct {
+	Name  string
+	Age   int
+	Alive bool
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	mem := Member{"Heo", 29, true}
+	jsonBytes, err := json.Marshal(mem)
+	if err != nil {
+		panic(err)
+	}
+
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Write(jsonBytes)
+	jsonString := string(jsonBytes)
+	fmt.Fprintf(w, jsonString)
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world")
@@ -22,6 +42,7 @@ func main() {
 	h := mux.NewRouter()
 	h.HandleFunc("/hello", hello).Methods("GET")
 	h.HandleFunc("/sum/{a}/{b}", sum).Methods("GET")
+	h.HandleFunc("/info", info).Methods("GET")
 	http.Handle("/", h)
 	http.ListenAndServe(":3000", nil)
 }
