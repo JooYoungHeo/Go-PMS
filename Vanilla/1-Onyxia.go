8a0c60a6725b684dd6c578b334bb9dce3b173baf
JooYoungHeo <jyhur900226@naver.com>
2018-08-12 00:01:01 +0900

    Act1-1 수정

--- Vanilla/1-Onyxia.go
+++ Vanilla/1-Onyxia.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func sing(n int) {
 	repetition := 1
-	pants := repetition * 10
-	sword := (repetition + 1) * 10
 
 	for repetition <= n {
+		pants := repetition * 10
+		sword := (repetition + 1) * 10
+
 		fmt.Printf("타잔이 %d원짜리 팬티를 입고, %d원짜리 칼을 차고 노래를 한다.\n", pants, sword)
 		repetition++
-		fmt.Println(repetition)
 	}
 }
 
