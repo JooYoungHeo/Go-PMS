80405f62734ead16edd340675897afe732725924
JooYoungHeo <jyhur900226@naver.com>
2018-08-18 23:54:28 +0900

    Act3-2

---
+++ 3-Lichking/2-Malygos.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func sortingSlice(list []int) []int {
+	len := len(list)
+	i := 0
+
+	for i < len-1 {
+		flag := true
+		target := list[i]
+		j := i + 1
+
+		for j < len {
+			to := list[j]
+
+			if target > to {
+				list[i], list[j] = list[j], list[i]
+				flag = false
+				break
+			}
+
+			j++
+		}
+
+		if flag {
+			i++
+		}
+	}
+
+	return list
+}
+
+func main() {
+	list := []int{4, 2, 3, 1}
+	sortedList := sortingSlice(list)
+
+	fmt.Println(sortedList)
+}
